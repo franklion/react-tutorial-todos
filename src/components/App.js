b2123143852b21e1658c7c0e6016d8bc686c4cdc
frank <frank.chen@skylinetw.com>
2018-12-14 18:06:03 +0800

    更新 問卷 example init & 切版

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react';
-import BmiInput from './BmiInput'
-import BmiResult from './BmiResult'
+import Cover from '../images/cover.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+// https://www.surveycake.com/s/Vxz86
+
 
 class App extends Component {
-
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      bmi: 0
-    }
-  }
-
   render() {
     return (
-      <div>
-        <h1>BMI PAGE</h1>
-        <BmiResult bmi={this.state.bmi} />
-        <BmiInput handleCalcBmi={this.handleCalcBmi}
-                  handleResetBmi={this.handleResetBmi}
-        />
+      <div className="container">
+        <div className="banner">
+          <img src={Cover} className="cover" alt="cover"/>
+        </div>
+        <div className="intro">
+          <h1 className="intro-title">公司午餐外送</h1>
+          <p className="intro-text">訂餐囉！今天要訂的是101食堂，大家想吃什麼呢？</p>
+        </div>
+        <div className="begin">
+          <span>開始</span>
+          <FontAwesomeIcon icon={['far', 'hand-point-down']} className="icon-hand-point-down" />
+          <span>填答</span>
+        </div>
+        {/* TODO: 麵包削 */}
+
+        <form className="survey-form">
+          <div className="subject-wrapper">
+            <h1 className="subject-title" data-index="1">今天想吃什麼</h1>
+            <div className="subject-answer">
+              <div className="option is-active">
+                <FontAwesomeIcon icon={['far', 'dot-circle']} className="icon-circle" />
+                排骨飯
+              </div>
+              <div className="option">
+                <FontAwesomeIcon icon={['far', 'circle']} className="icon-circle" />
+                排骨蛋炒飯
+              </div>
+              <div className="option">
+                <FontAwesomeIcon icon={['far', 'circle']} className="icon-circle" />
+                椒麻雞飯
+              </div>
+            </div>
+          </div>
+        </form>
+
+
       </div>
-    )
+    );
   }
-
-  handleCalcBmi = (height, weight) => {
-    const heightMeter = height / 100
-    const newBmi = +(weight / Math.pow(heightMeter, 2)).toFixed(1)
-    this.setState({ bmi: newBmi })
-  }
-
-  handleResetBmi = () => {
-    this.setState({ bmi: 0})
-  }
-
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './sass/index.sass'
 
+// third-party
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCircle, faHandPointDown, faDotCircle } from '@fortawesome/free-regular-svg-icons'
+
+library.add( faCircle, faHandPointDown, faDotCircle )
+
 ReactDOM.render(<App />, document.getElementById('root'));
